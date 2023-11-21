0057ff8a4ee3f2d256c4c672acb0891b33c6597f
Felipe Berteli <112442989+FBerteli@users.noreply.github.com>
2023-11-21 16:12:39 -0300

    Docs-Inicio-Projeto
    Adicionando os arquivos iniciais do projeto Spring

---
+++ Projeto-Zelda/src/test/java/br/com/sistemazelda/Projeto/Zelda/ProjetoZeldaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sistemazelda.Projeto.Zelda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoZeldaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
