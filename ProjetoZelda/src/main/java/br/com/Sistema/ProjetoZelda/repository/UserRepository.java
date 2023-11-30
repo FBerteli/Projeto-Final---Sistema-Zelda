3ad60d6bc2b5a0370acfceedcb3ca481f31ea089
Daishimomura1 <dai.shimomura2003@gmail.com>
2023-11-29 23:14:10 -0300

    zelda

--- ProjetoZelda/src/main/java/br/com/Sistema/ProjetoZelda/repository/UserRepository.java
+++ ProjetoZelda/src/main/java/br/com/Sistema/ProjetoZelda/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package br.com.Sistema.ProjetoZelda.repository;
 
 import br.com.Sistema.ProjetoZelda.Model.UserModel;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface UserRepository extends JpaRepository<UserModel, Long>{
+public interface UserRepository extends JpaRepository<UserModel, Long> {
 }
